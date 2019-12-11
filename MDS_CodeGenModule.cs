6d88afac3fd64e8e65c1ae123e9ad2fac70c8c0e
Gary S. Eades <gary.eades@inmechasol.com>
2019-12-10 21:48:06 -0500

    Initial Commit

---
+++ MDS_CodeGenModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDS_CodeGeneration
+{
+    public class MDS_CodeGenModule:MDS_Types_Tools.BaseClass_Module
+    {
+        public MDS_CodeGenModule(MDS_Types_Tools.BaseClass_ModuleExecutionSystem modExeSys) : base(modExeSys) { }
+    }
+}
